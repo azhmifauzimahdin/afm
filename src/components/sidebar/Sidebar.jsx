a801a6f4eb7e76294580a8046f54810520a28b21
azhmifauzimahdin <azhmifauzi11@gmail.com>
2023-12-27 16:46:13 +0700

    Create sidebars

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './Sidebar.css';
+import Logo from '/logo-afm.svg';
+import { menus } from '../../data/data';
+
+const Sidebar = () => {
+    const [toggle, setToggle] = useState(false);
+    return (
+        <>
+            <aside className={`aside ${toggle && "show-menu"}`}>
+                <a href="#home" className="nav__logo">
+                    <img src={Logo} alt="Logo" />
+                </a>
+
+                <nav className="nav">
+                    <div className="nav__menu">
+                        <ul className="nav__list">
+                            {menus.map((menu) => (
+                                <li className="nav__item" key={menu.id}>
+                                    <a href={menu.link} className="nav__link">
+                                        <i className={menu.icon}></i>
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </nav>
+
+                <div className="nav__footer">
+                    <span className="copyright">
+                        &copy; 2023 AFM
+                    </span>
+                </div>
+            </aside >
+
+            <div onClick={() => setToggle(!toggle)} className={`nav__toggle ${toggle && "nav__toggle-open"}`}>
+                <i className="icon-menu" ></i>
+            </div >
+        </>
+    )
+}
+
+export default Sidebar;
---
+++ src/data/data.js
@@ -0,0 +1,32 @@
+export const menus = [
+    {
+        id: 1,
+        link: "#home",
+        icon: "icon-home"
+    },
+    {
+        id: 2,
+        link: "#about",
+        icon: "icon-user-following"
+    },
+    {
+        id: 3,
+        link: "#resume",
+        icon: "icon-graduation"
+    },
+    {
+        id: 4,
+        link: "#skill",
+        icon: "icon-settings"
+    },
+    {
+        id: 5,
+        link: "#portfolio",
+        icon: "icon-folder-alt"
+    },
+    {
+        id: 6,
+        link: "#contact",
+        icon: "icon-bubble"
+    }
+] 
